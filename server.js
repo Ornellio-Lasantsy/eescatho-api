92efb732e7143784cb2c748a7a5428fa3fdda16a
Ornellio-Lasantsy <165698925+Ornellio-Lasantsy@users.noreply.github.com>
2025-06-30 13:56:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.json());
 
 // Configuration de la base de données
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "eescatho",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
